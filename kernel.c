7c1b47750b58755a71ee9113e2d5d3d3870f0f3c
ardririy <hynnkgw27@gmail.com>
2023-08-21 22:49:56 +0900

    記述ミスを修正

--- kernel.c
+++ kernel.c
@@ -18,13 +18,13 @@ void kernel_main(void){
     for(;;);
 }
 
-__attribute__((section(".text.boot")));
-__attribute__((naked));
+__attribute__((section(".text.boot")))
+__attribute__((naked))
 void boot(void){
-    __asm__ __volatile(
+    __asm__ __volatile__(
         "mv sp, %[stack_top]\n"
         "j kernel_main\n"
         :
-        :[stack_top] "r" (__stack_top)
+        : [stack_top] "r" (__stack_top)
     );
 }
